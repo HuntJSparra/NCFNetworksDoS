6b8b97b827e0c2af7882abe2f39c44e5e892a838
huntjsparra <huntehlaboratories@gmail.com>
2019-05-06 16:26:42 -0400

    Added research for the legality of DoSing and when it might be justified.

--- flasksite/what_is_dos.py
+++ flasksite/what_is_dos.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 from main import app
 
 @app.route('/what-is-dos')
-def dos_page():
-    return render_template('what-is-dos.html')
+def what_dos_page():
+    return render_template('what-is-dos/what-is-dos.html')
 
 @app.route('/what-is-dos/buffer-overflow')
 def buffer_overflow_page():
-    return render_template('buffer-overflow.html')
+    return render_template('what-is-dos/buffer-overflow.html')
 
 @app.route('/what-is-dos/ddos')
 def ddos_page():
-    return render_template('ddos.html')
+    return render_template('what-is-dos/ddos.html')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from flask import Flask
 app = Flask(__name__)
 
-from flasksite import landing, what_is_dos
+from flasksite import landing, what_is_dos, how_to_dos
